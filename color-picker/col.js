9bd8274c1934b1f318dd50a0f9d6f3dc51eeb4f0
sai <venkatasaikanderi@gmail.com>
2024-09-24 16:26:52 +0530

    color changing code

---
+++ color-picker/col.js
@@ -0,0 +1,24 @@
+
+let  ground=document.getElementById("back");
+let chan=document.getElementById("change");
+
+function bt(){
+   
+    ground.style.backgroundColor="#e0e0e0";
+    chan.textContent="#e0e0e0"
+}
+function bt1(){
+   
+    ground.style.backgroundColor="#6fcf97";
+    chan.textContent="#6fcf97"
+}
+function bt2(){
+    
+    ground.style.backgroundColor="#56ccf2";
+    chan.textContent="#56ccf2"
+}
+function bt3(){
+    
+    ground.style.backgroundColor="#bb6bd9";
+    chan.textContent="##bb6bd9"
+}
